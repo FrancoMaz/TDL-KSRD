97879a6518b968646986ec9986bb2c21181b5bd8
Franco Mazzoni <franco.mazzoni73@gmail.com>
2021-02-19 20:07:14 -0300

    Fix in comments title

--- tdl-ksrd/app/controllers/comments_shop_controller.rb
+++ tdl-ksrd/app/controllers/comments_shop_controller.rb
@@ -2,6 +2,5 @@ class CommentsShopController < ApplicationController
   def show
     @user = current_user
     @comments = current_user.comments.order('created_at desc')
-    @user_url = url_for controller: 'comments', action: 'new', shop: current_user.hash_for_url
   end
 end
